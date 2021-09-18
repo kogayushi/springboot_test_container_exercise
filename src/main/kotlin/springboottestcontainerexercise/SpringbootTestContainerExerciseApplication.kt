0d68dabbe819fdc35027ed4f9ca72acfb682c8d9
koga yushi <youshe.old.pleasure@gmail.com>
2021-09-18 13:18:51 +0900

    initial commit

---
+++ src/main/kotlin/springboottestcontainerexercise/SpringbootTestContainerExerciseApplication.kt
@@ -0,0 +1,11 @@
+package springboottestcontainerexercise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootTestContainerExerciseApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootTestContainerExerciseApplication>(*args)
+}
---
+++ src/test/kotlin/springboottestcontainerexercise/SpringbootTestContainerExerciseApplicationTests.kt
@@ -0,0 +1,13 @@
+package springboottestcontainerexercise
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootTestContainerExerciseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
